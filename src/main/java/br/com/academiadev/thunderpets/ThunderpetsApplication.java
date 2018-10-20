b116f70fd3084bd891b3451567f171e27f9224f0
rartner <ramon.artner@gmail.com>
2018-10-20 15:04:46 -0300

    setup project

---
+++ src/main/java/br/com/academiadev/thunderpets/ThunderpetsApplication.java
@@ -0,0 +1,12 @@
+package br.com.academiadev.thunderpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderpetsApplication.class, args);
+	}
+}
