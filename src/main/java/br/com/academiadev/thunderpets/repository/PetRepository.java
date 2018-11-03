a15a0c94699c99e10a93c06b9451d631c51f7d8f
DaMews <adammews@gmail.com>
2018-11-03 16:52:40 -0300

    Circle Ci - 2

--- src/main/java/br/com/academiadev/thunderpets/repository/PetRepository.java
+++ src/main/java/br/com/academiadev/thunderpets/repository/PetRepository.java
@@ -1,17 +1,11 @@
 package br.com.academiadev.thunderpets.repository;
 
-import br.com.academiadev.thunderpets.enums.*;
 import br.com.academiadev.thunderpets.model.Pet;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
