7894bbdd97c1fbc7e7d31545841901a5f6c26aba
Ramon Artner <ramon.artner@gmail.com>
2018-11-16 22:07:36 -0200

    adicionando permissão para todos os métodos

--- src/main/java/br/com/academiadev/thunderpets/config/security/CorsConfig.java
+++ src/main/java/br/com/academiadev/thunderpets/config/security/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("PUT", "DELETE", "OPTION", "GET", "POST");
     }
 }
