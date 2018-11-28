808ba767f7aa3648438f3f88180bbd6112442dab
Ramon Artner <ramon.artner@gmail.com>
2018-11-28 08:06:29 -0200

    importando Transactional

--- src/main/java/br/com/academiadev/thunderpets/controller/AuthController.java
+++ src/main/java/br/com/academiadev/thunderpets/controller/AuthController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.transaction.Transactional;
+
 @RestController
 @RequestMapping("oauth")
 @Transactional
