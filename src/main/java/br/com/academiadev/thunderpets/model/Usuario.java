7335bb588d2eadde126d78c06689577c16cc363c
Ramon Artner <ramon.artner@gmail.com>
2018-11-30 17:16:02 -0200

    corrigido flyway para realizar migrações no start do projeto

--- src/main/java/br/com/academiadev/thunderpets/model/Usuario.java
+++ src/main/java/br/com/academiadev/thunderpets/model/Usuario.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -38,8 +39,10 @@ public class Usuario implements UserDetails {
     @Size(min = 8)
     private String senha;
 
-    @Basic(fetch = FetchType.EAGER) @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    @Lob
     private byte[] foto;
+    
     private boolean ativo = true;
 
     @Override
