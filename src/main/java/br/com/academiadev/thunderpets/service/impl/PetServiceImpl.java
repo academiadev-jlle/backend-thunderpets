ca0710735adfe4c60a556ae01c52ff8bb4b5860f
Helena Dalmazo <helenadalmazo@outlook.com>
2018-12-03 11:47:02 -0200

    Identação corrigida conforme comentário do Ramon

--- src/main/java/br/com/academiadev/thunderpets/service/impl/PetServiceImpl.java
+++ src/main/java/br/com/academiadev/thunderpets/service/impl/PetServiceImpl.java
@@ -66,7 +66,7 @@ public class PetServiceImpl implements PetService {
                                Integer tamanho,
                                Sort.Direction direcao,
                                String campoOrdenacao,
-                                boolean ativo) {
+                               boolean ativo) {
 
         Localizacao localizacao = new Localizacao();
         if (tipoPesquisaLocalidade != null && tipoPesquisaLocalidade.equals(TipoPesquisaLocalidade.CIDADE_ESTADO)) {
